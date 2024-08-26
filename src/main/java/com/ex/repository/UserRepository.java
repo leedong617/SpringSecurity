795cfc2cd60ada7c7302d5c2589c7d7323a484f8
leedong617 <leedong617@gmail.com>
2024-08-27 00:39:36 +0900

    ..

--- src/main/java/com/ex/repository/UserRepository.java
+++ src/main/java/com/ex/repository/UserRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ex.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long>{
-	// username 중복 여부
+	// username 받아 DB 테이블에서 해당 username이 존재하는지
 	Boolean existsByUsername(String username);
-	// username으로 찾기
+	// username을 받아 DB 테이블에서 회원을 조회하는 메소드 작성
 	UserEntity findByUsername(String username);
 }
